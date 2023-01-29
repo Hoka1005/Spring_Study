636f7f24da6d52a9a9f301b7d1e271dd39fce51d
이호국 <lhg@LHG-MacBookPro.local>
2023-01-30 01:54:34 +0900

    Build : Create first project

---
+++ src/main/java/ASLAB/ASLABSpring/AslabSpringApplication.java
@@ -0,0 +1,13 @@
+package ASLAB.ASLABSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AslabSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AslabSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ASLAB/ASLABSpring/AslabSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ASLAB.ASLABSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AslabSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
